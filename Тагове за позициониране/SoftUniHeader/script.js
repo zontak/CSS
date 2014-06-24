c25fb32a3a45c3a24fb1e803ce8f70d77d99688d
Kristiyan <zontak@mail.bg>
2014-06-24 22:00:58 +0300

    First Question

---
+++ Тагове за позициониране/SoftUniHeader/script.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zonta_000 on 5.6.2014 г..
+ */
+var about=document.getElementById("aboutche");
+about.onmouseover=function(){
+    var list=document.getElementById("aboutTheUniversity");
+    list.style.display="block";
+}
+
+about.onmouseout=function(){
+    var list=document.getElementById("aboutTheUniversity");
+    list.style.display="none";
+}
+
+
+
+
