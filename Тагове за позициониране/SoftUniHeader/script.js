936938a55981e3b1f5f73939c60f046629500ab5
Kristiyan <zontak@mail.bg>
2014-07-01 19:45:17 +0300

    update

--- Тагове за позициониране/SoftUniHeader/script.js
+++ Тагове за позициониране/SoftUniHeader/script.js
@@ -1,14 +1,14 @@
 /**
  * Created by zonta_000 on 5.6.2014 г..
  */
-var about=document.getElementById("aboutche");
+var about=document.getElementById("aboutTheUniversity");
 about.onmouseover=function(){
-    var list=document.getElementById("aboutTheUniversity");
+    var list=document.getElementById("aboutche");
     list.style.display="block";
 }
 
 about.onmouseout=function(){
-    var list=document.getElementById("aboutTheUniversity");
+    var list=document.getElementById("aboutche");
     list.style.display="none";
 }
 
